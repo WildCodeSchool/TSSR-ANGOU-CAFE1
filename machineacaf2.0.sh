3c8e1c0ccecdcea638217941cebf2be6a4b17c63
Skchaper <axel.jm.charpentier@gmail.com>
2024-10-22 14:57:39 +0200

    Update machineacaf2.0.sh

--- machineacaf2.0.sh
+++ machineacaf2.0.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Présentation du menu
+
+# Renseignez tasse sinon gobelet
+
+# Introduisez les sous
+
+# Choix boisson
+
+# Choix sucre si 0 à 5 si > à 2 alors touillette
+
+# Préparation
+
+# Question renvoie au menu sinon paimenent / rendu monnaie
+
 
